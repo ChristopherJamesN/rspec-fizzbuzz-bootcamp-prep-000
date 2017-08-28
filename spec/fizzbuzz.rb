6b7e4fa1ec77556eaaa24f70de82abcf8c8847db
Chris Nady <nady.christopher@gmail.com>
2017-08-28 16:15:13 +0000

    automatically backed up by learn

--- spec/fizzbuzz.rb
+++ spec/fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(num)
   if num%3 == 0 && num%5 == 0
     puts "FizzBuzz"
-  else if num%5 == 0
+  elseif num%5 == 0
     puts "Buzz"
-  else if num%3 == 0
+  elseif num%3 == 0
     puts "Fizz"
   else
     puts nil
